fee0e6e98e2f69dbed53a6a31d37c76e4cdd1acc
ssping <514079588@qq.com>
2019-09-24 00:02:56 +0800

    add config page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
+// import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
 
 class Hello extends React.Component {
-    render() {
-       return (
-           <p>hello react！</p>
-        )
-    }
+  render() {
+    return <p>hello world</p>
+  }
 }
-render(
-    <Hello/>,
-    document.getElementById('root')
-)  
-
+render(<Hello />, document.getElementById("root"));
 
 if (module.hot) {
-    // 实现热更新
-    module.hot.accept();
-  }
+  module.hot.accept("./print.js", function() {
+    console.log("Accepting the updated printMe module!");
+    printMe();
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,7 @@ module.exports={
         path: path.resolve(__dirname, 'dist'),
         filename:'bundle.js'
     },
-    devtool: 'inline-source-map',
-    devServer:{
-        contentBase:"./src",
-        port: 3000,             // 端口
-        open: true,             // 自动打开浏览器
-        hot: true,               // 开启热更新
-        overlay: true, // 浏览器页面上显示错误
-        historyApiFallback: true
-    },
+    devtool: 'source-map',
     plugins:[
         new HtmlWebpackPlugin({
             // 用哪个html作为模板
@@ -28,8 +20,17 @@ module.exports={
             hash: true, // 会在打包好的bundle.js后面加上hash串
         }),
         new CleanWebpackPlugin(),
+        new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ],
+    devServer:{
+        contentBase:"./src",
+        port: 3000,             // 端口
+        open: true,             // 自动打开浏览器
+        hot: true,               // 开启热更新
+        overlay: true, // 浏览器页面上显示错误
+        historyApiFallback: true
+    },
     resolve: {
         // 别名
         alias: {
