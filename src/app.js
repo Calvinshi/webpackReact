4514f2aff92b18b3b6e6b31261f7db2deb8b5681
ssping <514079588@qq.com>
2019-10-15 23:19:53 +0800

    arrow function work by babel 0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 import { hot } from 'react-hot-loader';
 import React from 'react'
 import appcss from './app.css'
+import Progress from './animation'
 
 class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            data : 2 
+        }
+    }
+
+    confirm = () => {
+        this.props.closeAlert();
+      }
+    handleChange (e){
+        // this.state.data++
+        this.setState({data:this.state.data+1})
+        this.forceUpdate();
+        console.log(this.state)
+        this.setState({data:this.state.data+1})
+        this.forceUpdate();
+        console.log(this.state)
+        this.setState({data:this.state.data+1})
+        console.log(this.state)
+        this.forceUpdate();
+        // update right now 
+        // setTimeout(() => {
+        //     this.setState({data:this.state.data+1})
+        //     console.log(this.state)
+        // }, 1000);
+        // setTimeout(() => {
+        //     this.setState({data:this.state.data+1})
+        //     console.log(this.state)
+        // }, 1000);
+    }
+
     render(){
         return(
-            <div className = {appcss.blue}>hello world ....</div>
+            <div className = {appcss.blue}>hello world ....{this.state.data}
+                <div>
+                    <button onClick= {()=> this.handleChange()}>Button</button>
+                </div>
+                <Progress></Progress>
+            </div>
+            
         ) 
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports={
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['es2015', 'react'],
+                        presets: ['es2015', 'react', 'stage-0'],
                         plugins: ['react-hot-loader/babel'],
                     }
                 },
