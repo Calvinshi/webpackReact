989818e67d2cbeab814047604d76c0d97310185c
ssping <514079588@qq.com>
2019-10-26 22:42:40 +0800

    muti-switch
    muti-switch

--- src/index.js
+++ src/index.js
@@ -19,7 +19,13 @@ ReactDOM.render(
             <Route  path="/c/:aa/:bb" component={PageC} />
             <Redirect from = "/redirect" to="/b"></Redirect>
             <Route component = {Error} />
-
+        </Switch>
+        <Switch>
+            <Route exact path="/" component={PageB} />
+            <Route  path="/b" component={PageA} />
+            <Route  path="/c/:aa/:bb" component={PageC} />
+            <Redirect from = "/redirect" to="/b"></Redirect>
+            <Route component = {Error} />
         </Switch>
     </Router>,
     document.getElementById("root")
--- src/pageB.js
+++ src/pageB.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default class PageB extends React.Component{
+    
     render(){
+        console.log(this.props)
+
         return(
             <div>b默认页面</div>
         );
